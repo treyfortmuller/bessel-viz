89c4d3f5e53ff4df4e824aec026a826aeca98745
Trey Fortmuller <tfortmuller@anduril.com>
2022-02-21 03:44:11 -0800

    rm grid test

--- grid.py
+++
@@ -1,7 +0,0 @@
-from manim import *
-
-class ExampleBoxes(Scene):
-    def construct(self):
-        boxes=VGroup(*[Square() for s in range(0,6)])
-        boxes.arrange_in_grid(rows=2, buff=1)
-        self.add(boxes)
